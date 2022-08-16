e7bb04dc54472aec54ad2015019618510a67ce86
Mostafa Maki <mostafa_m9@yahoo.com>
2022-08-15 21:54:13 -0700

    saving today's work

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,8 @@
 
+var pollenData = document.getElementById("pollen-data")
+var grassIndex = document.getElementById("grass-index")
 function getApi() {
-    var requestUrl = ``;
+    var requestUrl = `https://api.purpleair.com/v1/sensors?fields=humidity,temperature&api_key=${api.purpAirKey}`;
   
     fetch(requestUrl)
       .then(function (response) {
@@ -8,6 +10,26 @@ function getApi() {
       })
       .then(function (data) {
         console.log(data)
+        // console.log(data.data[0])
+        // treeIndex.textContent = "hi"
+
       });
 }
-// getApi();
+getApi();
+function testApi() {
+    var requestUrl = `https://api.tomorrow.io/v4/timelines?location=33.8583,-118.0648&fields=treeIndex,grassIndex&apikey=${api.tomKey}`;
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+        console.log(data.data.timelines[0].intervals[0].values.grassIndex)
+        grassIndex.textContent = `grass index: ${data.data.timelines[0].intervals[0].values.grassIndex}`
+        // console.log(data.data[0])
+        // treeIndex.textContent = "hi"
+
+      });
+}
+testApi();
