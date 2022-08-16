554bca11af63359d8746276f20cdc6153377f57c
Mostafa Maki <mostafa_m9@yahoo.com>
2022-08-16 09:58:57 -0700

    included EPA AirNow API

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,8 @@
 
-var pollenData = document.getElementById("pollen-data")
-var grassIndex = document.getElementById("grass-index")
+// var pollenData = document.getElementById("pollen-data")
+var currentAQ = document.getElementById("current-aq")
+var category = document.getElementById("current-cat")
+var number = document.getElementById("current-num")
 function getApi() {
     var requestUrl = `https://api.purpleair.com/v1/sensors?fields=humidity,temperature&api_key=${api.purpAirKey}`;
   
@@ -9,7 +11,7 @@ function getApi() {
         return response.json();
       })
       .then(function (data) {
-        console.log(data)
+        // console.log(data)
         // console.log(data.data[0])
         // treeIndex.textContent = "hi"
 
@@ -32,4 +34,26 @@ function testApi() {
 
       });
 }
-testApi();
+// testApi();
+function airNowApi() {
+  var requestUrl = `https://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=39.0509&longitude=-121.4453&date=2022-08-16&distance=25&API_KEY=${api.airNowKey}`;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json(); 
+    })
+    .then(function (data) {
+      console.log(data)
+      //gives air quality
+      console.log(data[0].AQI)
+      currentAQ.textContent = 'Current Air Quality: ' + data[0].AQI;
+      //gives AQ a category name
+      console.log(data[0].Category.Name)
+      category.textContent = 'Quality: ' + data[0].Category.Name
+      // gives AQ a categry number
+      console.log(data[0].Category.Number)
+      number.textContent = 'Category: ' + data[0].Category.Number
+
+    });
+}
+airNowApi();
