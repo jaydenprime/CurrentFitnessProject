951d95724b97411ab82fdda0bb5526cb0dd8d0fe
Mostafa Maki <mostafa_m9@yahoo.com>
2022-08-16 19:26:56 -0700

    updated script to work when search button is clicked

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,6 +5,11 @@ var number = document.getElementById("current-num")
 var searchBtn = document.getElementById("search-btn")
 var searchInput = document.getElementById("search-input")
 
+var storedLat = localStorage.getItem("latitude")
+var storedLon = localStorage.getItem("longtitude")
+
+
+
 searchBtn.addEventListener("click", function() {
   var cityInput = searchInput.value;
   var cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&appid=${api.openWeaKey}`;
@@ -31,8 +36,33 @@ searchBtn.addEventListener("click", function() {
         console.log(storedLat)
         
         console.log(storedLon)
-          })
 
+        airNowApi();
+
+        function airNowApi() {
+          var requestUrl = `https://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=${storedLat}&longitude=${storedLon}&date=2022-08-16&distance=25&API_KEY=${api.airNowKey}`;
+        
+          fetch(requestUrl)
+            .then(function (response) {
+              return response.json(); 
+            })
+            .then(function (data) {
+              console.log(data)
+              //gives air quality
+              console.log(data[0].AQI)
+              currentAQ.textContent = 'Current Air Quality: ' + data[0].AQI;
+              //gives AQ a category name
+              console.log(data[0].Category.Name)
+              category.textContent = 'Quality: ' + data[0].Category.Name
+              // gives AQ a categry number
+              console.log(data[0].Category.Number)
+              number.textContent = 'Category: ' + data[0].Category.Number
+        
+            });
+        }
+
+          })
+          
 }
 })
 
@@ -50,7 +80,9 @@ function getApi() {
 
       });
 }
+
 getApi();
+
 function testApi() {
     var requestUrl = `https://api.tomorrow.io/v4/timelines?location=33.8583,-118.0648&fields=treeIndex,grassIndex&apikey=${api.tomKey}`;
   
@@ -67,47 +99,3 @@ function testApi() {
 
       });
 }
-// testApi();
-function airNowApi() {
-  var requestUrl = `https://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=39.0509&longitude=-121.4453&date=2022-08-16&distance=25&API_KEY=${api.airNowKey}`;
-
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json(); 
-    })
-    .then(function (data) {
-      console.log(data)
-      //gives air quality
-      console.log(data[0].AQI)
-      currentAQ.textContent = 'Current Air Quality: ' + data[0].AQI;
-      //gives AQ a category name
-      console.log(data[0].Category.Name)
-      category.textContent = 'Quality: ' + data[0].Category.Name
-      // gives AQ a categry number
-      console.log(data[0].Category.Number)
-      number.textContent = 'Category: ' + data[0].Category.Number
-
-    });
-}
-airNowApi();
-
-function showWeather() {
-  var cityInput = input.val();
-  if (!cityInput) {
-      alert("Please Enter a Valid City Name")
-      return;
-  }
-  cityEl.text(cityInput);
-  var cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&appid=${api.key}`;
-  searchedCities.push(cityInput);
-  localStorage.setItem("city", searchedCities);
-  var lastCity = searchedCities[searchedCities.length - 1];
-  var searchEl = $('<li>');
-  searchEl.attr("class", "list-group-item list-group-item-dark li-custom")
-  searchEl.text(lastCity)
-  searchHistory.append(searchEl);
-
-}
-
-
-
