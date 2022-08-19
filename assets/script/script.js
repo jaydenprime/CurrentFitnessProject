394bbd6a48dd175118079ff3a2e3aad0a1fa9c01
jPrime <jaydenprime@gmail.com>
2022-08-18 18:47:47 -0700

    test

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
 
 function getApi() {
-    var requestUrl = ``;
+    var requestUrl = `https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today`;
   
     fetch(requestUrl)
       .then(function (response) {
